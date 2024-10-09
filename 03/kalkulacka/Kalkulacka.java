987abffe30eb0afbcdda50b4339932743401b19b
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-09 14:00:34 +0200

    03 implementacia kalkulacky

--- 03/kalkulacka/Kalkulacka.java
+++ 03/kalkulacka/Kalkulacka.java
@@ -1,30 +1,32 @@
 public class Kalkulacka {
+    private double vysledok;
     
     public Kalkulacka() {
-        
+        this.vysledok = 0;
     }
     
     public void zadaj(double hodnota) {
-        
+        this.vysledok = hodnota;
     }
     
     public void plus(double hodnota) {
-        
+        // this.vysledok = this.vysledok + hodnota;
+        this.vysledok += hodnota;
     }
     
     public void minus(double hodnota) {
-        
+        this.vysledok -= hodnota;
     }
     
     public void krat(double hodnota) {
-        
+        this.vysledok *= hodnota;
     }
     
     public void deleno(double hodnota) {
-        
+        this.vysledok /= hodnota;
     }
     
     public double getVysledok() {
-        return -1; // TODO
+        return this.vysledok;
     }
 }
