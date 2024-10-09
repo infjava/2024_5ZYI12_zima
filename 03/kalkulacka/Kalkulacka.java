5d58e664a33f43445e5507dfba6e8d9d86216c84
Jan Janech <jan.janech@fri.uniza.sk>
2024-10-09 13:55:25 +0200

    03 zaklad kalkulacky v jave podla UML

---
+++ 03/kalkulacka/Kalkulacka.java
@@ -0,0 +1,30 @@
+public class Kalkulacka {
+    
+    public Kalkulacka() {
+        
+    }
+    
+    public void zadaj(double hodnota) {
+        
+    }
+    
+    public void plus(double hodnota) {
+        
+    }
+    
+    public void minus(double hodnota) {
+        
+    }
+    
+    public void krat(double hodnota) {
+        
+    }
+    
+    public void deleno(double hodnota) {
+        
+    }
+    
+    public double getVysledok() {
+        return -1; // TODO
+    }
+}
