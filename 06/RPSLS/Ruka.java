0dc816f37265ae6f4790b0278d9198ef1e11e733
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-06 14:31:19 +0100

    06 zaklad pre triedu Ruka

---
+++ 06/RPSLS/Ruka.java
@@ -0,0 +1,17 @@
+public class Ruka {
+    public Ruka(int x, int y) {
+        
+    }
+    
+    public void nasledujuceGesto() {
+        
+    }
+    
+    public void predchadzajuceGesto() {
+        
+    }
+    
+    public void setGesto(int cislo) {
+        
+    }
+}
