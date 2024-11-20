61574a0a89a65d65d2c4c9d5b13f73784ddc1cf4
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-20 14:22:19 +0100

    08 policko sa da zasiahnut a moze obsahovat referenciu na lodku

---
+++ 08/03 Lodicky/Policko.java
@@ -0,0 +1,30 @@
+import fri.shapesge.Stvorec;
+
+public class Policko {
+    private Stvorec stvorec;
+    private Lodicka lodicka;
+    private boolean trafene;
+    
+    public Policko(int x, int y) {
+        this.stvorec = new Stvorec(x * 20, y * 20);
+        this.stvorec.zmenStranu(20);
+        this.stvorec.zmenFarbu("blue");
+        this.stvorec.zobraz();
+        
+        this.lodicka = null;
+        this.trafene = false;
+    }
+    
+    public void postavLodicku(Lodicka lodicka) {
+        this.lodicka = lodicka;
+    }
+    
+    public void zasiahni() {
+        this.trafene = true;
+        if (this.lodicka == null) {
+            this.stvorec.zmenFarbu("#C0C0C0");
+        } else {
+            this.stvorec.zmenFarbu("red");
+        }
+    }
+}
