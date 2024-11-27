072088247471ef3af6ad4ccab3fe33b7b070c5af
Jan Janech <jan.janech@fri.uniza.sk>
2024-11-27 15:28:05 +0100

    09 vypis tabulky vysledkov

--- 09/turnaje/Turnaj.java
+++ 09/turnaje/Turnaj.java
@@ -41,4 +41,41 @@ public class Turnaj {
         this.tabulka[domaci][hostia] = vysledok;
         return true;
     }
+    
+    public void vypisTabulku() {
+        System.out.format("%20s", "");
+        
+        for (Tim tim : this.prihlaseneTimy) {
+            System.out.format("%20s", tim.getNazovTimu());
+        }
+        
+        System.out.println();
+        
+        for (int domaci = 0; domaci < this.prihlaseneTimy.length; domaci++) {
+            System.out.format("%20s", this.prihlaseneTimy[domaci].getNazovTimu());
+            
+            for (int hostia = 0; hostia < this.prihlaseneTimy.length; hostia++) {
+                System.out.format("%20s", this.tabulka[domaci][hostia]);
+            }
+            
+            System.out.println();
+        }
+        
+        if (this.uzavrety) {
+            System.out.println("Turnaj bol uzavrety!");
+            
+            int[] bodyTimov = new int[this.prihlaseneTimy.length];
+            
+            for (int domaci = 0; domaci < this.prihlaseneTimy.length; domaci++) {
+                for (int hostia = 0; hostia < this.prihlaseneTimy.length; hostia++) {
+                    bodyTimov[domaci] += this.tabulka[domaci][hostia].getBodyDomaci();
+                    bodyTimov[hostia] += this.tabulka[domaci][hostia].getBodyHostia();
+                }
+            }
+            
+            for (int tim = 0; tim < this.prihlaseneTimy.length; tim++) {
+                System.out.format("%20s%5d%n", this.prihlaseneTimy[tim].getNazovTimu(), bodyTimov[tim]);
+            }
+        }
+    }
 }
