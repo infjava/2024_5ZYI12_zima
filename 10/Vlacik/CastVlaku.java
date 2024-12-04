34e8a2c789cf6a219f6cbd9e6799d65bc53feda5
Jan Janech <jan.janech@fri.uniza.sk>
2024-12-04 15:20:36 +0100

    10 posuvanie CastiVlaku

--- 10/Vlacik/CastVlaku.java
+++ 10/Vlacik/CastVlaku.java
@@ -1,6 +1,8 @@
 import fri.shapesge.Obrazok;
 
 public class CastVlaku {
+    private static final int VELKOST = 35;
+    
     private Poloha poloha;
     private final Obrazok obrazok;
     private CastVlaku dalsiaCast;
@@ -28,10 +30,24 @@ public class CastVlaku {
     
     public void pripojCast(TypCastiVlaku typNovejCasti) {
         if (this.dalsiaCast == null) {
-            Poloha novaPoloha = this.poloha.getPosunutaPoloha(-35, this.poloha.getSmer());
+            Poloha novaPoloha = this.poloha.getPosunutaPoloha(-CastVlaku.VELKOST, this.poloha.getSmer());
             this.dalsiaCast = new CastVlaku(novaPoloha, typNovejCasti);
         } else {
             this.dalsiaCast.pripojCast(typNovejCasti);
         }
+    }
+    
+    public void pohniNaNovuPoziciu(Smer smer) {
+        this.pohniNaNovuPoziciu(
+            this.poloha.getPosunutaPoloha(CastVlaku.VELKOST, smer)
+        );
+    }
+    
+    private void pohniNaNovuPoziciu(Poloha novaPoloha) {
+        if (this.dalsiaCast != null) {
+            this.dalsiaCast.pohniNaNovuPoziciu(this.poloha);
+        }
+        
+        this.setPoloha(novaPoloha);
     }
 }
